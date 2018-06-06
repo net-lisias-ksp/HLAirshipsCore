e7cf159029cf6ae4490aac309b9257cc6ac03993
Lisias T <me@lisias.net>
2018-06-05 22:01:55 -0300

    Normalizing versioning.

---
+++ HLAirships/Version.cs
@@ -0,0 +1,12 @@
+﻿namespace HLAirships
+{
+	static class Version
+	{
+		public const int major = 5;
+		public const int minor = 6;
+		public const int patch = 1;
+		public const int build = 1;
+		public const string Number = "5.6.1.1";
+		public const string Text = Number + " Unofficial";
+	}
+}
