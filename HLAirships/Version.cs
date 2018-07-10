7dc051d103e2d423881f12db9c66ee04864c791b
Lisias T <me@lisias.net>
2018-07-10 00:16:30 -0300

    Unofficial Version 5.6.1.2 : Recompile for 1.4

--- HLAirships/Version.cs
+++ HLAirships/Version.cs
@@ -5,8 +5,8 @@
 		public const int major = 5;
 		public const int minor = 6;
 		public const int patch = 1;
-		public const int build = 1;
-		public const string Number = "5.6.1.1";
+		public const int build = 2;
+		public const string Number = "5.6.1.2";
 		public const string Text = Number + " Unofficial";
 	}
 }
--- pack.sh
+++ pack.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-VERSION=$(cat ./Source/Version.cs | grep -Po 'public const string Number = "(.+)";' | sed 's/public const string Number = "//' | sed 's/";//')
+VERSION=$(cat ./HLAirships/Version.cs | grep -Po 'public const string Number = "(.+)";' | sed 's/public const string Number = "//' | sed 's/";//')
 echo $VERSION
 FILE=HLAirships-$VERSION.zip
 rm $FILE
 zip -r $FILE ./GameData/* -x ".*"
 zip -d $FILE __MACOSX .DS_Store
 mv $FILE ./Archive
+echo Done for $VERSION
