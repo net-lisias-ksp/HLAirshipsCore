12e6a0863382325082644748a4d69a72dead6a5c
Lisias <me@lisias.net>
2020-12-21 03:21:11 -0300

    Adding Startup

---
+++ Source/HLAirships/Startup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace HLAirships
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	internal class Startup : MonoBehaviour
+	{
+		private void Start()
+		{
+			Log.init();
+			Log.log("Version {0} running on KSP {1}", Version.Text, KSPe.Util.KSP.Version.Current.ToString());
+
+			try
+			{
+				KSPe.Util.Installation.Check<Startup>();
+			}
+			catch (KSPe.Util.InstallmentException e)
+			{
+				Log.err(e.ToShortMessage());
+				KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+			}
+		}
+	}
+}
