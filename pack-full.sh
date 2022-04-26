c79466740f8c729b2cc6f294ac5cdb06d64f5503
Lisias T <me@lisias.net>
2022-04-26 11:06:38 -0300

    Automation scripts update

--- pack-full.sh
+++ pack-full.sh
@@ -15,8 +15,7 @@ FILE=${pwd}/Archive/$PACKAGE-$VERSION${PROJECT_STATE}.zip
 echo $FILE
 clean
 zip -r $FILE ./GameData/* -x ".*"
-zip -r $FILE ./PluginData/* -x ".*"
-zip -r $FILE ./Extras/* -x ".*"
+zip -r $FILE ./Ships/* -x ".*"
 zip $FILE INSTALL.md
 zip -d $FILE __MACOSX "**/.DS_Store"
 cd $pwd
